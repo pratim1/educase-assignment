703465e1d77c3ee6ca85b5ab43f92d2b13da57cf
pratim1 <pratimdas784114@gmail.com>
2025-08-22 16:57:28 +0530

    fixed deployment issue

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -6,7 +6,7 @@ function Layout({ children, className = "" }) {
     
       <div
         className={`font-rubik container flex flex-col bg-gray-50 border border-gray-200 
-        max-w-md w-[375px] gap-6 p-6 h-screen overflow-y-auto ${className}`}
+        max-w-md w-[375px] gap-6 p-6 min-h-screen  ${className}`}
       >
         {children}
       </div>
